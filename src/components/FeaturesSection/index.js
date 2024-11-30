e20ed3c1c33aa0dfbe15ad0c92c7e7647166fe26
Mahima <mahimajyothi234@gmail.com>
2024-11-30 06:04:36 +0000

    Final Code

---
+++ src/components/FeaturesSection/index.js
@@ -0,0 +1,15 @@
+import Playtime from '../Playtime'
+import NewWaysToConnect from '../NewWaysToConnect'
+
+import './index.css'
+
+const FeaturesSection = () => {
+  return (
+    <div className="features-section-container">
+      <Playtime />
+      <NewWaysToConnect/>
+    </div>
+  )
+}
+
+export default FeaturesSection
